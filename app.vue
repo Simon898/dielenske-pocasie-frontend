15e67fd9fff677e655f9b3013f70cd895fcc5e45
Simon Horvath <simonh898@gmail.com>
2022-09-12 14:36:19 +0200

    update: Nuxt2 to Nuxt3

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    <!-- <NuxtWelcome /> -->
+    <div>
     <h1 class="text-center">Dielenske</h1>
     <h3 class="text-center">Pocasie</h3>
     <v-card width="600" class="mx-auto pa-3">
-      <v-row justify="center" align="center">
+      <v-row justify="center">
       <v-icon x-large color="blue">mdi-account-circle</v-icon>
       </v-row>
       <v-card-title class="justify-center">Login</v-card-title>
@@ -16,15 +18,13 @@
         <v-text>Sign up here</v-text>
         <br />
       </v-card-actions>
-      <v-row justify="center" align="center">
+      <v-row justify="center">
       <v-btn color="primary" class="text-center mb-2">Login</v-btn>
       </v-row>
     </v-card>
   </div>
+  </div>
 </template>
-
 <script>
-export default {}
-</script>
 
-<style></style>
+</script>
